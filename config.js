658ceb87465c4a457a6a19b39ed2b2806ac521ea
shvam0000 <shvam0000@gmail.com>
2020-09-22 18:51:20 +0530

    config

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "fe1cfbd5a6a0cfb0352dbdcaf6a52e38"
+    }
+}
+
+module.exports = constants;
